7456831b92831358d7e72a0458332a2f33a2faf5
Roman Zeyde <me@romanzey.de>
2019-01-21 14:37:56 +0200

    Log more information in case of an invalid block during bulk load

--- src/bulk.rs
+++ src/bulk.rs
@@ -1,4 +1,4 @@
-use bitcoin::blockdata::block::Block;
+use bitcoin::blockdata::block::{Block, BlockHeader};
 use bitcoin::consensus::encode::{deserialize, Decodable};
 use bitcoin::util::hash::{BitcoinHash, Sha256dHash};
 use libc;
@@ -120,6 +120,7 @@ pub fn parse_blocks(blob: Vec<u8>, magic: u32) -> Result<Vec<Block>> {
         match u32::consensus_decode(&mut cursor) {
             Ok(value) => {
                 if magic != value {
+                    trace!("bad magic @ {}", pos);
                     cursor
                         .seek(SeekFrom::Start(pos + 1))
                         .expect("failed to seek back");
@@ -145,6 +146,16 @@ pub fn parse_blocks(blob: Vec<u8>, magic: u32) -> Result<Vec<Block>> {
                     end - start,
                     err
                 );
+                let header_size = std::mem::size_of::<BlockHeader>();
+                if start + header_size <= end {
+                    let header: BlockHeader =
+                        deserialize(&blob[start..start + header_size]).expect("invalid header");
+                    error!(
+                        "header of {}: {:?}",
+                        header.bitcoin_hash().be_hex_string(),
+                        header
+                    );
+                }
                 cursor
                     .seek(SeekFrom::Start(pos + 1))
                     .expect("failed to seek back");
