db8322b81059f0203326c58854cbc21b0d224e82
willcl-ark <will8clark@gmail.com>
2021-03-11 11:45:20 +0000

    WIP: Re-adding functions back in

--- src/rpc.rs
+++ src/rpc.rs
@@ -3,7 +3,7 @@ use bitcoin::consensus::encode::{deserialize, serialize};
 use bitcoin::hashes::hex::{FromHex, ToHex};
 use bitcoin::hashes::{sha256d::Hash as Sha256dHash, Hash};
 use error_chain::ChainedError;
-use serde_cbor::{to_vec, Value};
+use serde_cbor::{de, to_vec, Value};
 use serde_derive::{Deserialize, Serialize};
 use std::collections::{HashMap, BTreeMap};
 use std::io::{BufRead, BufReader, Write};
@@ -52,12 +52,209 @@ impl Connection {
         }
     }
 
+    // fn blockchain_headers_subscribe(&mut self) -> Result<Value> {
+    //     let entry = self.query.get_best_header()?;
+    //     let hex_header = hex::encode(serialize(entry.header()));
+    //     let result = json!({"hex": hex_header, "height": entry.height()});
+    //     self.last_header_entry = Some(entry);
+    //     Ok(result)
+    // }
+
     fn server_version(&self, params: &[serde_json::value::Value]) -> Result<serde_cbor::Value> {
         if params.len() != 2 {
             bail!("invalid params: {:?}", params);
         }
         Ok(serde_cbor::value::to_value(format!("{}, {}", ELECTRS_VERSION, PROTOCOL_VERSION)).unwrap())
     }
+
+    // fn server_banner(&self) -> Result<Value> {
+    //     Ok(json!(self.query.get_banner()?))
+    // }
+    //
+    // fn server_donation_address(&self) -> Result<Value> {
+    //     Ok(Value::Null)
+    // }
+    //
+    // fn server_peers_subscribe(&self) -> Result<Value> {
+    //     Ok(json!([]))
+    // }
+    //
+    // fn mempool_get_fee_histogram(&self) -> Result<Value> {
+    //     Ok(json!(self.query.get_fee_histogram()))
+    // }
+    //
+    // fn blockchain_block_header(&self, params: &[Value]) -> Result<Value> {
+    //     let height = usize_from_value(params.get(0), "height")?;
+    //     let cp_height = usize_from_value_or(params.get(1), "cp_height", 0)?;
+    //
+    //     let raw_header_hex: String = self
+    //         .query
+    //         .get_headers(&[height])
+    //         .into_iter()
+    //         .map(|entry| hex::encode(&serialize(entry.header())))
+    //         .collect();
+    //
+    //     if cp_height == 0 {
+    //         return Ok(json!(raw_header_hex));
+    //     }
+    //     let (branch, root) = self.query.get_header_merkle_proof(height, cp_height)?;
+    //
+    //     let branch_vec: Vec<String> = branch.into_iter().map(|b| b.to_hex()).collect();
+    //
+    //     Ok(json!({
+    //         "header": raw_header_hex,
+    //         "root": root.to_hex(),
+    //         "branch": branch_vec
+    //     }))
+    // }
+    //
+    // fn blockchain_block_headers(&self, params: &[Value]) -> Result<Value> {
+    //     let start_height = usize_from_value(params.get(0), "start_height")?;
+    //     let count = usize_from_value(params.get(1), "count")?;
+    //     let cp_height = usize_from_value_or(params.get(2), "cp_height", 0)?;
+    //     let heights: Vec<usize> = (start_height..(start_height + count)).collect();
+    //     let headers: Vec<String> = self
+    //         .query
+    //         .get_headers(&heights)
+    //         .into_iter()
+    //         .map(|entry| hex::encode(&serialize(entry.header())))
+    //         .collect();
+    //
+    //     if count == 0 || cp_height == 0 {
+    //         return Ok(json!({
+    //             "count": headers.len(),
+    //             "hex": headers.join(""),
+    //             "max": 2016,
+    //         }));
+    //     }
+    //
+    //     let (branch, root) = self
+    //         .query
+    //         .get_header_merkle_proof(start_height + (count - 1), cp_height)?;
+    //
+    //     let branch_vec: Vec<String> = branch.into_iter().map(|b| b.to_hex()).collect();
+    //
+    //     Ok(json!({
+    //         "count": headers.len(),
+    //         "hex": headers.join(""),
+    //         "max": 2016,
+    //         "root": root.to_hex(),
+    //         "branch" : branch_vec
+    //     }))
+    // }
+
+    // fn blockchain_estimatefee(&self, params: &[Value]) -> Result<Value> {
+    //     let blocks_count = usize_from_value(params.get(0), "blocks_count")?;
+    //     let fee_rate = self.query.estimate_fee(blocks_count); // in BTC/kB
+    //     Ok(json!(fee_rate.max(self.relayfee)))
+    // }
+    //
+    // fn blockchain_relayfee(&self) -> Result<Value> {
+    //     Ok(json!(self.relayfee)) // in BTC/kB
+    // }
+    //
+    // fn blockchain_scripthash_subscribe(&mut self, params: &[Value]) -> Result<Value> {
+    //     let script_hash =
+    //         hash_from_value::<Sha256dHash>(params.get(0)).chain_err(|| "bad script_hash")?;
+    //     let status = self.query.status(&script_hash[..])?;
+    //     let result = status.hash().map_or(Value::Null, |h| json!(hex::encode(h)));
+    //     if self
+    //         .status_hashes
+    //         .insert(script_hash, result.clone())
+    //         .is_none()
+    //     {
+    //         self.stats.subscriptions.inc();
+    //     }
+    //
+    //     Ok(result)
+    // }
+    //
+    // fn blockchain_scripthash_get_balance(&self, params: &[Value]) -> Result<Value> {
+    //     let script_hash =
+    //         hash_from_value::<Sha256dHash>(params.get(0)).chain_err(|| "bad script_hash")?;
+    //     let status = self.query.status(&script_hash[..])?;
+    //     Ok(
+    //         json!({ "confirmed": status.confirmed_balance(), "unconfirmed": status.mempool_balance() }),
+    //     )
+    // }
+    //
+    // fn blockchain_scripthash_get_history(&self, params: &[Value]) -> Result<Value> {
+    //     let script_hash =
+    //         hash_from_value::<Sha256dHash>(params.get(0)).chain_err(|| "bad script_hash")?;
+    //     let status = self.query.status(&script_hash[..])?;
+    //     Ok(json!(Value::Array(
+    //         status
+    //             .history()
+    //             .into_iter()
+    //             .map(|item| item.to_json())
+    //             .collect()
+    //     )))
+    // }
+    //
+    // fn blockchain_scripthash_listunspent(&self, params: &[Value]) -> Result<Value> {
+    //     let script_hash =
+    //         hash_from_value::<Sha256dHash>(params.get(0)).chain_err(|| "bad script_hash")?;
+    //     Ok(unspent_from_status(&self.query.status(&script_hash[..])?))
+    // }
+    //
+    // fn blockchain_transaction_broadcast(&self, params: &[Value]) -> Result<Value> {
+    //     let tx = params.get(0).chain_err(|| "missing tx")?;
+    //     let tx = tx.as_str().chain_err(|| "non-string tx")?;
+    //     let tx = hex::decode(&tx).chain_err(|| "non-hex tx")?;
+    //     let tx: Transaction = deserialize(&tx).chain_err(|| "failed to parse tx")?;
+    //     let txid = self.query.broadcast(&tx)?;
+    //     self.query.update_mempool()?;
+    //     if let Err(e) = self.sender.try_send(Message::PeriodicUpdate) {
+    //         warn!("failed to issue PeriodicUpdate after broadcast: {}", e);
+    //     }
+    //     Ok(json!(txid.to_hex()))
+    // }
+    //
+    // fn blockchain_transaction_get(&self, params: &[Value]) -> Result<Value> {
+    //     let tx_hash = hash_from_value(params.get(0)).chain_err(|| "bad tx_hash")?;
+    //     let verbose = match params.get(1) {
+    //         Some(value) => value.as_bool().chain_err(|| "non-bool verbose value")?,
+    //         None => false,
+    //     };
+    //     Ok(self.query.get_transaction(&tx_hash, verbose)?)
+    // }
+    //
+    // fn blockchain_transaction_get_confirmed_blockhash(&self, params: &[Value]) -> Result<Value> {
+    //     let tx_hash = hash_from_value(params.get(0)).chain_err(|| "bad tx_hash")?;
+    //     self.query.get_confirmed_blockhash(&tx_hash)
+    // }
+    //
+    // fn blockchain_transaction_get_merkle(&self, params: &[Value]) -> Result<Value> {
+    //     let tx_hash = hash_from_value(params.get(0)).chain_err(|| "bad tx_hash")?;
+    //     let height = usize_from_value(params.get(1), "height")?;
+    //     let (merkle, pos) = self
+    //         .query
+    //         .get_merkle_proof(&tx_hash, height)
+    //         .chain_err(|| "cannot create merkle proof")?;
+    //     let merkle: Vec<String> = merkle.into_iter().map(|txid| txid.to_hex()).collect();
+    //     Ok(json!({
+    //             "block_height": height,
+    //             "merkle": merkle,
+    //             "pos": pos}))
+    // }
+    //
+    // fn blockchain_transaction_id_from_pos(&self, params: &[Value]) -> Result<Value> {
+    //     let height = usize_from_value(params.get(0), "height")?;
+    //     let tx_pos = usize_from_value(params.get(1), "tx_pos")?;
+    //     let want_merkle = bool_from_value_or(params.get(2), "merkle", false)?;
+    //
+    //     let (txid, merkle) = self.query.get_id_from_pos(height, tx_pos, want_merkle)?;
+    //
+    //     if !want_merkle {
+    //         return Ok(json!(txid.to_hex()));
+    //     }
+    //
+    //     let merkle_vec: Vec<String> = merkle.into_iter().map(|entry| entry.to_hex()).collect();
+    //
+    //     Ok(json!({
+    //         "tx_hash" : txid.to_hex(),
+    //         "merkle" : merkle_vec}))
+    // }
 
     fn handle_command(&mut self, method: &str, params: &[serde_json::Value], id: &serde_json::Value) -> serde_cbor::Result<Value> {
         let timer = self
@@ -66,6 +263,28 @@ impl Connection {
             .with_label_values(&[method])
             .start_timer();
         let result: Result<serde_cbor::Value> = match method {
+            // "blockchain.block.header" => self.blockchain_block_header(&params),
+            // "blockchain.block.headers" => self.blockchain_block_headers(&params),
+            // "blockchain.estimatefee" => self.blockchain_estimatefee(&params),
+            // "blockchain.headers.subscribe" => self.blockchain_headers_subscribe(),
+            // "blockchain.relayfee" => self.blockchain_relayfee(),
+            // "blockchain.scripthash.get_balance" => self.blockchain_scripthash_get_balance(&params),
+            // "blockchain.scripthash.get_history" => self.blockchain_scripthash_get_history(&params),
+            // "blockchain.scripthash.listunspent" => self.blockchain_scripthash_listunspent(&params),
+            // "blockchain.scripthash.subscribe" => self.blockchain_scripthash_subscribe(&params),
+            // "blockchain.transaction.broadcast" => self.blockchain_transaction_broadcast(&params),
+            // "blockchain.transaction.get" => self.blockchain_transaction_get(&params),
+            // "blockchain.transaction.get_merkle" => self.blockchain_transaction_get_merkle(&params),
+            // "blockchain.transaction.get_confirmed_blockhash" => {
+            //     self.blockchain_transaction_get_confirmed_blockhash(&params)
+            // }
+            // "blockchain.transaction.id_from_pos" => {
+            //     self.blockchain_transaction_id_from_pos(&params)
+            // }
+            // "mempool.get_fee_histogram" => self.mempool_get_fee_histogram(),
+            // "server.banner" => self.server_banner(),
+            // "server.donation_address" => self.server_donation_address(),
+            // "server.peers.subscribe" => self.server_peers_subscribe(),
             "server.ping" => Ok(Value::Null),
             "server.version" => self.server_version(&params),
             &_ => Err(Error::from("bad command")),
@@ -137,22 +356,15 @@ impl Connection {
 
     fn parse_requests(mut reader: BufReader<TcpStream>, tx: SyncSender<Message>) -> Result<()> {
         loop {
-            let mut line = Vec::<u8>::new();
-            reader
-                .read_until(b'\n', &mut line)
-                .chain_err(|| "failed to read a request")?;
-            if line.is_empty() {
+            // Read a cbor value from a reader
+			let request: BTreeMap<Value, Value> = serde_cbor::from_reader(&reader)?;
+            if request.is_empty() {
                 tx.send(Message::Done).chain_err(|| "channel closed")?;
                 return Ok(());
             } else {
-                if line.starts_with(&[22, 3, 1]) {
-                    // (very) naive SSL handshake detection
-                    let _ = tx.send(Message::Done);
-                    bail!("invalid request - maybe SSL-encrypted data?: {:?}", line)
-                }
-                match String::from_utf8(line) {
-                    Ok(req) => tx
-                        .send(Message::Request(req))
+                match request {
+                    Ok(request) => tx
+                        .send(Message::Request(request.get(&Value::from("request"))))
                         .chain_err(|| "channel closed")?,
                     Err(err) => {
                         let _ = tx.send(Message::Done);
@@ -160,6 +372,29 @@ impl Connection {
                     }
                 }
             }
+            // let mut line = Vec::<u8>::new();
+            // reader
+            //     .read_until(b'\n', &mut line)
+            //     .chain_err(|| "failed to read a request")?;
+            // if line.is_empty() {
+            //     tx.send(Message::Done).chain_err(|| "channel closed")?;
+            //     return Ok(());
+            // } else {
+            //     if line.starts_with(&[22, 3, 1]) {
+            //         // (very) naive SSL handshake detection
+            //         let _ = tx.send(Message::Done);
+            //         bail!("invalid request - maybe SSL-encrypted data?: {:?}", line)
+            //     }
+            //     match String::from_utf8(line) {
+            //         Ok(req) => tx
+            //             .send(Message::Request(req))
+            //             .chain_err(|| "channel closed")?,
+            //         Err(err) => {
+            //             let _ = tx.send(Message::Done);
+            //             bail!("invalid UTF8: {}", err)
+            //         }
+            //     }
+            // }
         }
     }
 
@@ -187,7 +422,7 @@ impl Connection {
 
 #[derive(Debug)]
 pub enum Message {
-    Request(String),
+    Request(Value),
     PeriodicUpdate,
     Done,
 }
